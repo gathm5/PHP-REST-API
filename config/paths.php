d8593d73e155c616fb7dc8ff6db211978c7905aa
gautham stalin <gauthamstalin@gmail.com>
2014-10-24 09:08:23 -0700

    start

---
+++ config/paths.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: archanagautham
+ * Date: 10/24/2014
+ * Time: 9:06 AM
+ */ 
---
+++ libs/Bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+class Bootstrap
+{
+
+    function __construct()
+    {
+        $url = $_GET["url"];
+        if (!isset($_GET["url"]) || strrpos($_GET["url"], "404")) {
+            $url = 'index';
+        }
+        $url = array_filter(explode("/", $url));
+        if (!file_exists("controllers/$url[0].php")) {
+            require "controllers/error.php";
+            $controller = new Error();
+            return false;
+        }
+        require "controllers/$url[0].php";
+        $controller = new $url[0](Controller::$DESKTOPVIEW);
+        $controller->loadModel($url[0]);
+        if (isset($url[2]) && method_exists($controller, $url[1])) {
+            $controller->{$url[1]}($url[2]);
+        } else if (isset($url[1]) && method_exists($controller, $url[1])) {
+            $controller->{$url[1]}();
+        } else {
+            $controller->index();
+        }
+    }
+
+}
